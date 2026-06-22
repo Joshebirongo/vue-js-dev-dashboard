88e89f91b391fcd7b641b2cc3ed5257375667512
Jonathan Shebirongo <jonathanshebirongo07@gmail.com>
2026-06-22 17:54:54 +0200

    Components created

---
+++ src/components/ProfileCard.vue
@@ -0,0 +1,28 @@
+/*
+* Inside the script tag we declare the imports
+* and the component logic.
+*/
+<script setup>
+    const developer = {
+        name: "Jonathan Shebirongo",
+        role: "Full Stack Developer",
+        country: "DR CONGO "
+    };
+
+</script>
+/*
+* Inside the tempate tag we declare the HTML
+* element inside the component.
+*/
+<template>
+    <div class="profile-card">
+        <img 
+        src="https://litslink.com/wp-content/uploads/2024/06/8-1024x682.jpg" 
+        alt="Profile picture"
+    >
+
+    <h2>{{ developer.name }}</h2>
+    <p>{{ developer.role }}</p>
+    <span>{{ developer.country }}</span>
+    </div>
+</template>

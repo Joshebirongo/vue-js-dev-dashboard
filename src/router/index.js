50cfdab2405f09a13a8042bfc380fb5cdcbf5da8
Jonathan Shebirongo <jonathanshebirongo07@gmail.com>
2026-06-24 17:33:57 +0200

    The message app router created

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+// Import the View.js router
+import { createRouter, createWebHistory } from "vue-router";
+// Import the pages
+import DashboardView from "../pages/DashboardView.vue";
+import ProjectView from "../pages/ProjectView.vue";
+
+const router = createRouter(
+    {
+        history: createWebHistory(),
+
+        routes: [
+            {
+                path: "/",
+                component: DashboardView
+            },
+            {
+                path: "/projects",
+                component: ProjectView
+            }
+        ]
+    }
+);
+
+export default router;

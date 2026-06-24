7f13078ba4aab6017965f925bc755356147fa83d
Jonathan Shebirongo <jonathanshebirongo07@gmail.com>
2026-06-24 17:35:21 +0200

    Main component updated

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
+// Import the main function to create the Vue application
 import { createApp } from 'vue'
+// Import the main App component
 import App from './App.vue'
+//Import the Vue application router
+import router from "./router/index.js"
+// Import the router from "./router"
 
 import "./styles/App.css"
 
-createApp(App).mount('#app')
+/* 
+    *Create the Vue application inside the HTML element 
+    *with the id 'app' and use the Vue.js router
+*/
+
+// CreateApp(App).use(router).mount('#app)
+
+createApp(App)
+    .use(router)
+    .mount('#app');

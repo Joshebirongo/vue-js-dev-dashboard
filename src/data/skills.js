a376d3a99a5e6cd286b10a0b0562a75ca4d2c0ac
Jonathan Shebirongo <jonathanshebirongo07@gmail.com>
2026-06-26 20:07:51 +0200

    Data skills created

---
+++ src/data/skills.js
@@ -0,0 +1,74 @@
+export const skills = [
+  {
+    id: 1,
+    name: "HTML",
+    category: "Frontend",
+    level: "Intermediate"
+  },
+  {
+    id: 2,
+    name: "CSS",
+    category: "Frontend",
+    level: "Advanced"
+  },
+  {
+    id: 3,
+    name: "JavaScript",
+    category: "Frontend",
+    level: "Advanced"
+  },
+  {
+    id: 4,
+    name: "React",
+    category: "Frontend",
+    level: "Intermediate"
+  },
+  {
+    id: 5,
+    name: "Vue.js",
+    category: "Frontend",
+    level: "Beginner"
+  },
+  {
+    id: 6,
+    name: "Node.js",
+    category: "Backend",
+    level: "Intermediate"
+  },
+  {
+    id: 7,
+    name: "Express.js",
+    category: "Backend",
+    level: "Intermediate"
+  },
+  {
+    id: 8,
+    name: "PHP",
+    category: "Backend",
+    level: "Beginner"
+  },
+  {
+    id: 9,
+    name: "Docker",
+    category: "DevOps",
+    level: "Beginner"
+  },
+  {
+    id: 10,
+    name: "Git & GitHub",
+    category: "DevOps",
+    level: "Intermediate"
+  },
+   {
+    id: 11,
+    name: "MySQL",
+    category: "Database",
+    level: "Intermediate"
+  },
+  {
+    id: 12,
+    name: "MongoDB",
+    category: "Database",
+    level: "Beginner"
+  },
+];

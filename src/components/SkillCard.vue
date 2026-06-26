203d6d1deeb8caf8f6634732db29ceb345ab4f88
Jonathan Shebirongo <jonathanshebirongo07@gmail.com>
2026-06-26 20:07:18 +0200

    Component Skillcard created

---
+++ src/components/SkillCard.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps(
+    {
+        skill: Object
+    }
+);
+
+</script>
+
+<template>
+    <div class="tech-card">
+        <h3>{{ skill.name }}</h3>
+        <p>{{ skill.category }}</p>
+        <span>{{ skill.level }}</span>
+    </div>
+</template>
